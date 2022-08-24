9d34f3c6a682dec7ea0002f9d85fa189b9f880db
sikethedev <ivoinestrachan242@gmail.com>
2022-08-24 18:28:17 -0400

    sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import styles from "../styles/sponsor.module.css"
 import wolframe from "../assets/wolframe.svg"
-
+import desmos from "../assets/desmos.svg"
+import echo3d from "../assets/echo3d.svg"
 const Sponsors = () => {
   return (
     <div>
        <h2 style={{color: 'white'}}className={styles.heading}>Sponsors</h2>
        <div className={styles.sponsorGrid }>
-       <div className={styles.sponsor}>
+            <div className={styles.sponsor}>
+              <a rel="noreferrer" href="https://www.desmos.com" target="_blank">
+                <img src={desmos} alt="desmos" width="200"></img>
+              </a>
+            </div>
+            <div className={styles.sponsor}>
               <a rel="noreferrer" href="https://www.wolframalpha.com/" target="_blank">
                 <img src={wolframe} alt="wolframe" width="318"></img>
+              </a>
+            </div>
+            <div className={styles.sponsor}>
+              <a rel="noreferrer" href="https://www.desmos.com" target="_blank">
+                <img src={echo3d} alt="echo" width="200"></img>
               </a>
             </div>
             </div>
