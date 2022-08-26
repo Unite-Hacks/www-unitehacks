fd5258db0a08875b59b1ec11bbc1f4020b8b502b
sikethedev <ivoinestrachan242@gmail.com>
2022-08-26 15:31:46 -0400

    plane

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import uparrow from "../assets/UpArrow.svg"
 import plane from "../assets/plane.svg"
 import Confetti from 'react-confetti'
 import useWindowSize from 'react-use/lib/useWindowSize'
+import planemob from "../assets/planemob.svg"
 
 const Header = () => {
   const { width, height } = useWindowSize()
@@ -24,7 +25,12 @@ const Header = () => {
   />
  
       <div>
+      <div className={styles.planemob}>
+        <img src={planemob} alt={planemob} draggable="false" onMouseEnter={() => handleShow(true)}
+        onMouseLeave={() => handleShow(false)}/>
+        </div>
       <div className={styles.plane}>
+        
       <img src={plane} alt={plane} draggable="false" onMouseEnter={() => handleShow(true)}
       onMouseLeave={() => handleShow(false)}/>
       </div>
