98d0e7eec1e3904979538ea2cb032462c0e3bb7e
Manish674 <manishchoudhary4905@gmail.com>
2022-08-31 18:09:42 +0530

    working on font-sizes

--- components/Time.js
+++ components/Time.js
@@ -1,5 +1,35 @@
 const DateTime = () => {
-  return <div>Date time</div>;
+  return (
+    <>
+      <div className="w-c mx-auto text-white flex justify-between ">
+        <div className="flex flex-col border-[8px] border-white p-4 justify-center items-center rounded-md">
+          <span className="font-bold italic text-h">OCTOBER</span>
+          <span className="text-h-xxxl  font-extrabold text-outline-bold text-transparent">
+            21
+          </span>
+          <span className="font-bold italic text-h">@ 6:00 PM</span>
+        </div>
+        <div className="flex flex-col border-[8px] border-white justify-center p-3 items-center rounded-md">
+          <span className="font-bold italic text-h">OCTOBER</span>
+          <span className="text-h-xxxl font-extrabold text-outline-bold text-transparent">
+            23
+          </span>
+          <span className="font-bold italic text-3xl text-h">@ 12:00 PM</span>
+        </div>
+      </div>
+      <div className="w-c mx-auto flex justify-center rounded-md bg-white">
+        <div className="w-c border-2 border-yellow-200">
+          <h2 className="text-sh-xl font-extrabold italic g-heading">
+            date & time
+          </h2>
+          <p className="text-h-p">
+            Doors will open at 6:00 PM on October 21st and close at 12:00 PM on
+            October 23rd.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DateTime;
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,23 @@ export default function Home() {
     <>
       <div className="bg-white w-[95%] mx-auto rounded-md flex flex-col justify-center items-center">
         <div className="mt-4 mb-12">
-          <h2 className="font-extrabold text-2xl ">
+          <h2 className="font-extrabold text-2xl text-h">
             JOIN US & UNITE THE <br />
             HACKER COMMUNITY AT
           </h2>
-          <h1 className="font-extrabold text-[2.55rem]  g-heading">
+          <h1 className="font-extrabold text-h-xl  g-heading">
             UNITE HACKS
           </h1>
           <div className="space-y-4">
             <div className="g-bg rounded-md p-1">
               <div className="flex space-x-4 py-1 justify-center h-[90%] bg-white g-border rounded-md border-black ">
                 <img src={locationicon.src} />
-                <span className="font-bold italic text-2xl">
+                <span className="font-bold italic text-sh">
                   SAN FRANCISCO, CA
                 </span>
               </div>
             </div>
-            <button className="g-bg-reverse p-1 py-1 w-full font-bold  text-transparent italic text-2xl rounded-md">
+            <button className="g-bg-reverse p-1 py-1 w-full font-bold  text-transparent italic text-sh rounded-md">
               <span className="text-outline">REGISTER FOR</span>{" "}
               <span className="text-white">UNITE!</span>
             </button>
