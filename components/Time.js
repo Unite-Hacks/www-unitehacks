baceff1a25af6979c14eb1b28b20a7f64b9e3e21
Manish <manishchoudhary4905@gmail.com>
2022-09-05 10:42:16 +0530

    testing grid for layout at the end of the page

--- components/Time.js
+++ components/Time.js
@@ -1,7 +1,9 @@
+import venue from "../public/assets/venue.png";
+
 const DateTime = () => {
   return (
-    <>
-      <div className="w-c mx-auto text-white flex justify-between mt-5">
+    <div className="grid md:grid-layout">
+      <div className="w-c md:w-[100%] dates mx-auto text-white flex justify-between mt-5">
         <div className="flex flex-col border-[5px] border-white p-4 justify-center items-center rounded-md">
           <span className="font-bold italic text-h">OCTOBER</span>
           <span className="text-h-xxxl  font-extrabold text-outline-bold text-transparent">
@@ -17,8 +19,8 @@ const DateTime = () => {
           <span className="font-bold italic text-3xl text-h">@ 12:00 PM</span>
         </div>
       </div>
-      <div className="w-c mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c border-2 border-white">
+      <div className="w-c md:w-[100%] time mx-auto flex justify-center rounded-md bg-white mt-5">
+        <div className="w-c md:w-[100%] border-2 border-white">
           <h2 className="text-sh-xl font-extrabold italic g-heading">
             date & time
           </h2>
@@ -28,8 +30,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c">
+      <div className="w-c md:w-[100%] venue mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
+        <div className="w-c md:w-[100%]">
           <h2 className="text-sh-xl font-extrabold italic text-white">
             the venue
           </h2>
@@ -39,8 +41,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c border-2 border-white">
+      <div className="w-c md:w-[100%] guidelines mx-auto flex justify-center rounded-md bg-white mt-5">
+        <div className="w-c md:w-[100%] border-2 border-white">
           <h2 className="text-sh-xl font-extrabold italic g-heading">
             covid-19
           </h2>
@@ -50,8 +52,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c">
+      <div className="w-c md:w-[100%] eligibility mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
+        <div className="w-c md:w-[100%]">
           <h2 className="text-sh-xl font-extrabold italic text-white">
             eligibility
           </h2>
@@ -61,28 +63,11 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-    </>
+      <div className="hidden md:block venue-img">
+        <img src={venue.src} />
+      </div>
+    </div>
   );
 };
 
 export default DateTime;
-
-{
-  /* <div className="mt-4 w-c mx-auto flex space-x-4 text-white"> */
-}
-{
-  /*   <div className="bg-transparent border-[3px] w-full border-white "> */
-}
-{
-  /*     Oc{" "} */
-}
-{
-  /*   </div> */
-}
-
-{
-  /*   <div className="bg-transparent border-[3px] w-full border-white "></div> */
-}
-{
-  /* </div> */
-}
--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,22 @@ import Sponsor from "../components/Sponsor";
 import Team from "../components/Team";
 import DateTime from "../components/Time";
 import locationicon from "../public/assets/locationicon.svg";
+import earth from "../public/assets/earth.jpg";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-white w-[95%] mx-auto rounded-md flex flex-col justify-center items-center mt-10">
+      <div className="md:pt-8 md:flex-row bg-white w-[95%] mx-auto rounded-md flex flex-col justify-center items-center mt-10">
         <div className="mt-4 mb-12">
           <h2 className="font-extrabold text-2xl text-h">
             JOIN US & UNITE THE <br />
             HACKER COMMUNITY AT
           </h2>
-          <h1 className="font-extrabold text-h-xl  g-heading">
-            UNITE HACKS
-          </h1>
+          <h1 className="font-extrabold text-h-xl  g-heading">UNITE HACKS</h1>
           <div className="space-y-4">
             <div className="g-bg rounded-md p-1">
-              <div className="flex space-x-4 py-1 justify-center h-[90%] bg-white g-border rounded-md border-black ">
-                <img src={locationicon.src}  alt="location"/>
+              <div className="flex space-x-4 py-1 justify-center h-[90%] bg-white rounded-md border-black ">
+                <img src={locationicon.src} alt="location" />
                 <span className="font-bold italic text-sh">
                   SAN FRANCISCO, CA
                 </span>
@@ -33,6 +32,17 @@ export default function Home() {
               <span className="text-white">UNITE!</span>
             </button>
           </div>
+        </div>
+        <div className="flex flex-col justify-center items-center w-[40%]">
+          <div className="g-bg w-[70%] rounded-md p-1 max-w-[213px]">
+            <div className="hidden bg-white md:flex flex-col p-4 justify-center items-center rounded-md">
+              <span className="font-bold italic text-h">OCTOBER</span>
+              <span className="text-h-xxxl text-black  font-extrabold text-outline-bold text-transparent">
+                21
+              </span>
+            </div>
+          </div>
+          <img className="w-[80%] max-w-[265px]" src={earth.src} />
         </div>
       </div>
       <DateTime />
