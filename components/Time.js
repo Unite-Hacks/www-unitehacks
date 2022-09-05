e81c898eb27d905641105a29b30ca5c169dcea06
inferno <100568769+Infernus007@users.noreply.github.com>
2022-09-05 21:55:26 +0530

    Update Time.js

--- components/Time.js
+++ components/Time.js
@@ -1,73 +1,81 @@
-import venue from "../public/assets/venue.png";
-
+const Mystyle = {
+  backgroundPosition: "center  center",
+  objectFit: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundImage: `url("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg")`
+};
 const DateTime = () => {
   return (
-    <div className="w-c mx-auto grid-layout">
-      <div className="w-c  dates mx-auto text-white flex justify-between mt-5">
-        <div className="flex flex-col border-[5px] border-white p-4 justify-center items-center rounded-md">
-          <span className="font-bold italic text-h">OCTOBER</span>
-          <span className="text-h-xxxl  font-extrabold text-outline-bold text-transparent">
-            21
-          </span>
-          <span className="font-bold italic text-h">@ 6:00 PM</span>
+    <>
+      <div className="w-[95%] mx-auto flex flex-col sm:grid sm:grid-cols-2 grid-row-flow-1fr  sm:gap-3">
+        <div className="flex justify-center order-3 sm:-order-none rounded-md border-[5px]  col-span-1 p-3 border-white mt-5">
+          <div className="w-c">
+            <h2 className="text-sh-xl font-extrabold italic text-white">
+              the venue
+            </h2>
+            <p className="text-h-p text-white">
+              Somewhere in San Francisco, California, or around the Bay Area! Will
+              be finalized soon.
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col border-[5px] border-white justify-center p-3 items-center rounded-md">
-          <span className="font-bold italic text-h">OCTOBER</span>
-          <span className="text-h-xxxl font-extrabold text-outline-bold text-transparent">
-            23
-          </span>
-          <span className="font-bold italic text-3xl text-h">@ 12:00 PM</span>
+
+        <div className=" sm:flex hidden border-white border-[5px] justify-center rounded-md mt-5">
+          <div className="max-w-full  w-full border-2 " style={Mystyle} />
+
         </div>
-      </div>
-      <div className="w-c  time mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c  border-2 border-white">
-          <h2 className="text-sh-xl font-extrabold italic g-heading">
-            date & time
-          </h2>
-          <p className="text-h-p">
-            Doors will open at 6:00 PM on October 21st and close at 12:00 PM on
-            October 23rd.
-          </p>
+        <div className=" flex justify-center order-2 sm:-order-none p-3  rounded-md bg-white mt-5">
+          <div className="w-c border-2 border-white">
+            <h2 className="text-sh-xl font-extrabold italic g-heading">
+              date & time
+            </h2>
+            <p className="text-h-p">
+              Doors will open at 6:00 PM on October 21st and close at 12:00 PM on
+              October 23rd.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="w-c  venue mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c ">
-          <h2 className="text-sh-xl font-extrabold italic text-white">
-            the venue
-          </h2>
-          <p className="text-h-p text-white">
-            Somewhere in San Francisco, California, or around the Bay Area! Will
-            be finalized soon.
-          </p>
+        <div className=" text-white flex text-3xl order-1 sm:-order-none justify-between mt-5">
+          <div className="flex flex-col w-[49%]  border-[5px]  border-white p-4 justify-center items-center rounded-md">
+            <span className="font-bold italic ">OCTOBER</span>
+            <span className=" font-extraboldx text-7xl text-outline-bold text-transparent">
+              21
+            </span>
+            <span className="font-bold italic ">@ 6:00 PM</span>
+          </div>
+          <div className="flex flex-col w-[49%] border-[5px] border-white justify-center p-3 items-center rounded-md">
+            <span className="font-bold italic ">OCTOBER</span>
+            <span className="font-extrabold text-outline-bold text-7xl text-transparent">
+              23
+            </span>
+            <span className="font-bold italic text-3xl text-h">@ 12:00 PM</span>
+          </div>
         </div>
-      </div>
-      <div className="w-c  guidelines mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c  border-2 border-white">
-          <h2 className="text-sh-xl font-extrabold italic g-heading">
-            covid-19
-          </h2>
-          <p className="text-h-p">
-            COVID-19 vaccination and proof of a negative test 24 hours prior is
-            required.
-          </p>
+          <div className="flex justify-center p-3 order-5 sm:-order-none rounded-md border-[5px]  col-span-1 border-white mt-5">
+            <div className="w-c ">
+              <h2 className="text-sh-xl font-extrabold italic text-white">
+                eligibility
+              </h2>
+              <p className="text-h-p text-white">
+                All high school & upper middle school students are welcome to join
+                us at Unite!
+              </p>
+            </div>
+          </div>
+          <div className=" flex justify-center order-4 sm:-order-none p-3 rounded-md bg-white mt-5">
+            <div className="w-c border-2 border-white">
+              <h2 className="text-sh-xl font-extrabold italic g-heading">
+                covid-19
+              </h2>
+              <p className="text-h-p">
+                COVID-19 vaccination and proof of a negative test 24 hours prior is
+                required.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="w-c  eligibility mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c">
-          <h2 className="text-sh-xl font-extrabold italic text-white">
-            eligibility
-          </h2>
-          <p className="text-h-p text-white">
-            All high school & upper middle school students are welcome to join
-            us at Unite!
-          </p>
-        </div>
-      </div>
-      <div className="hidden md:block  venue-img">
-        <img src={venue.src} className="w-[100%] h-[100%] object-fill" />
-      </div>
-    </div>
-  );
+      </>
+      );
 };
 
-export default DateTime;
+      export default DateTime;
