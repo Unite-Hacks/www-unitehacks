721f7d032f89f1ded9f3bc721bd7c6579836a35b
Manish <manishchoudhary4905@gmail.com>
2022-09-05 18:00:52 +0530

    got the grid-layout class working

--- components/Time.js
+++ components/Time.js
@@ -2,8 +2,8 @@ import venue from "../public/assets/venue.png";
 
 const DateTime = () => {
   return (
-    <div className="grid md:grid-layout">
-      <div className="w-c md:w-[100%] dates mx-auto text-white flex justify-between mt-5">
+    <div className="w-c mx-auto grid-layout">
+      <div className="w-c  dates mx-auto text-white flex justify-between mt-5">
         <div className="flex flex-col border-[5px] border-white p-4 justify-center items-center rounded-md">
           <span className="font-bold italic text-h">OCTOBER</span>
           <span className="text-h-xxxl  font-extrabold text-outline-bold text-transparent">
@@ -19,8 +19,8 @@ const DateTime = () => {
           <span className="font-bold italic text-3xl text-h">@ 12:00 PM</span>
         </div>
       </div>
-      <div className="w-c md:w-[100%] time mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c md:w-[100%] border-2 border-white">
+      <div className="w-c  time mx-auto flex justify-center rounded-md bg-white mt-5">
+        <div className="w-c  border-2 border-white">
           <h2 className="text-sh-xl font-extrabold italic g-heading">
             date & time
           </h2>
@@ -30,8 +30,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c md:w-[100%] venue mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c md:w-[100%]">
+      <div className="w-c  venue mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
+        <div className="w-c ">
           <h2 className="text-sh-xl font-extrabold italic text-white">
             the venue
           </h2>
@@ -41,8 +41,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c md:w-[100%] guidelines mx-auto flex justify-center rounded-md bg-white mt-5">
-        <div className="w-c md:w-[100%] border-2 border-white">
+      <div className="w-c  guidelines mx-auto flex justify-center rounded-md bg-white mt-5">
+        <div className="w-c  border-2 border-white">
           <h2 className="text-sh-xl font-extrabold italic g-heading">
             covid-19
           </h2>
@@ -52,8 +52,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="w-c md:w-[100%] eligibility mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
-        <div className="w-c md:w-[100%]">
+      <div className="w-c  eligibility mx-auto flex justify-center rounded-md border-[5px] border-white mt-5">
+        <div className="w-c">
           <h2 className="text-sh-xl font-extrabold italic text-white">
             eligibility
           </h2>
@@ -63,8 +63,8 @@ const DateTime = () => {
           </p>
         </div>
       </div>
-      <div className="hidden md:block venue-img">
-        <img src={venue.src} />
+      <div className="hidden md:block  venue-img">
+        <img src={venue.src} className="w-[100%] h-[100%] object-fill" />
       </div>
     </div>
   );
