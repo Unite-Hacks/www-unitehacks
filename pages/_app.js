fd6af3c0e0f819f015320e5784f155310f0a18c9
sikethedev <ivoinestrachan242@gmail.com>
2022-09-10 21:30:25 -0400

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Meta from '../components/Meta'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Meta />
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
