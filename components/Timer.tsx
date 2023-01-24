97080410bc3cb6f7842c78cf911742cc80d90e88
sikethedev <ivoinestrachan242@gmail.com>
2023-01-23 20:50:05 -0500

    added postcard and timer

---
+++ components/Timer.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const Timer = () => {
+  const [timer, setTimer] = useState();
+  const [day, setDay] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [hr, setHr] = useState(0);
+
+  const countDownDate = new Date("03/31/2023 9:00:00 PST").getTime();
+
+  setInterval(() => {
+    var today = new Date().getTime();
+
+    var distance = countDownDate - today;
+    setDay(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    setHour(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    setMin(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    setSec(Math.floor((distance % (1000 * 60)) / 1000));
+  }, 1000);
+  return (
+    <div>
+      <div className="flex gap-4 text-center justify-center bg-white sm:h-[17vh] sm:mt-5 h-20 pt-5 mt-2">
+        <div>
+          <p className="sm:text-[52px] font-bold time">{day}</p>
+          <p className="font-bold">Days</p>
+        </div>
+        <div>
+          <p className="sm:text-[52px] font-bold time">{hour}</p>
+          <p className="font-bold">Hours</p>
+        </div>
+        <div>
+          <p className="sm:text-[52px] font-bold time">{min}</p>
+          <p className="font-bold">Mins</p>
+        </div>
+        <div>
+          <p className="sm:text-[52px] font-bold time">{sec}</p>
+          <p className="font-bold">Secs</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,68 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import hero from "../assets/hero.svg";
+import Timer from "../components/Timer";
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+import stamp from "../assets/stamp.svg";
 
 const Home: NextPage = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <>
       <Head>
         <title>Hackers Unite!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
-  )
-}
+      <div>
+        <div>
+          {isHovered ? (
+            <div className="sm:bg-white sm:w-[1262px] ml-auto mr-auto mt-20 cursor-pointer h-[772.88px] rounded-[20px]">
+              <div className="text-center text-[32px] pt-5">POSTCARD</div>
+              <div className="flex justify-end w-[95%]">
+                <Image src={stamp} alt="stamp" />
+              </div>
+              <div className="mt-[60px] flex items-center justify-between w-[95%]">
+                <div>
+                  <Image src={logo} alt="logo" />
+                </div>
+                <div className="text-center h-[180px] border border-blue-700"></div>
+                <div className="text-left mr-[150px]">
+                  <div className="border-b-2 border-black w-40">WHERE:</div>
 
-export default Home
+                  <div className="border-b-2 border-black w-40 mt-5">
+                    Bay Area
+                  </div>
+
+                  <div className="border-b-2 border-black w-40 mt-5">WHEN:</div>
+
+                  <div className="border-b-2 border-black w-40 mt-5">April</div>
+                </div>
+              </div>
+              <div></div>
+            </div>
+          ) : (
+            <div className="relative">
+              <Image
+                src={hero}
+                alt="hero"
+                className="ml-auto mr-auto mt-20 cursor-pointer"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              />
+              <div className="absolute bottom-10 right-0 p-2 text-white w-[40%]">
+                <p className="font-bold sm:text-[42px]">WELCOME TO</p>
+                <p className="font-bold sm:text-[52px] unite">UNITE HACKS</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <Timer />
+    </>
+  );
+};
+
+export default Home;
