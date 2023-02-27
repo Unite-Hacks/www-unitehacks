e4702af31c50cb85df96cf7889b63fe76a0b8e46
Manish <manishchoudhary4905@gmail.com>
2023-02-27 10:10:12 +0530

    increased font size of hero component. Created sepearte file for hero component

---
+++ components/Flipper.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+import hero from "../assets/hero.svg";
+import logo from "../assets/logo.svg";
+import stamp from "../assets/stamp.svg";
+
+const Flipper = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div>
+      <div className="w-[95%] mx-auto mt-4 ">
+        {isHovered ? (
+          <div className="sm:bg-white sm:w-[1262px] ml-auto mr-auto mt-4 cursor-pointer h-[772.88px] rounded-[20px]"
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div className="text-center text-[32px] pt-5">POSTCARD</div>
+            <div className="flex justify-end w-[95%]">
+              <Image src={stamp} alt="stamp" />
+            </div>
+
+            <div className="mt-[60px] mx-auto  flex items-center justify-between w-[95%]">
+              <div>
+                <Image src={logo} alt="logo" />
+              </div>
+              <div className="text-center h-[250px] border-2 bordersplit rounded-sm"></div>
+              <div className="text-left mr-[150px] gloria">
+                <div className="border-b-2 border-black w-40 ">WHERE:</div>
+
+                <div className="border-b-2 border-black w-40 mt-5 ">
+                  Bay Area
+                </div>
+
+                <div className="border-b-2 border-black w-40 mt-5 ">WHEN:</div>
+
+                <div className="border-b-2 border-black w-40 mt-5 ">April</div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="relative ">
+            <Image
+              src={hero}
+              alt="hero"
+              className="ml-auto mr-auto cursor-pointer"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            />
+            <div className="absolute bottom-0 right-0 translate-y-[-55%] p-2 text-white w-[55%]">
+              <p className="font-bold text-xl sm:text-[45px] paytone">WELCOME TO</p>
+              <p className="font-bold text-[1.3rem] sm:text-[55px] unite paytone">UNITE HACKS</p>
+            </div>
+          </div>
+        )}
+
+      </div>
+    </div>
+  )
+}
+
+export default Flipper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,73 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import hero from "../assets/hero.svg";
 import Timer from "../components/Timer";
-import { useState } from "react";
-import logo from "../assets/logo.svg";
-import stamp from "../assets/stamp.svg";
 import Community from "../components/Community";
 import Schedule from "../components/Schedule";
 import Team from "../components/Team";
 import Footer from "../components/Footer";
 import Sponsors from "../components/Sponsors";
 import PrevHack from "../components/PrevHack";
+import Flipper from "../components/Flipper";
 
 const Home: NextPage = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
     <>
       <Head>
         <title>Hackers Unite!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <div className="pb-8 w-[95%] mx-auto mt-4 ">
-          {isHovered ? (
-            <div className="sm:bg-white sm:w-[1262px] ml-auto mr-auto mt-4 cursor-pointer h-[772.88px] rounded-[20px]"
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <div className="text-center text-[32px] pt-5">POSTCARD</div>
-              <div className="flex justify-end w-[95%]">
-                <Image src={stamp} alt="stamp" />
-              </div>
-
-              <div className="mt-[60px] mx-auto  flex items-center justify-between w-[95%]">
-                <div>
-                  <Image src={logo} alt="logo" />
-                </div>
-                <div className="text-center h-[250px] border-2 bordersplit rounded-sm"></div>
-                <div className="text-left mr-[150px] gloria">
-                  <div className="border-b-2 border-black w-40 ">WHERE:</div>
-
-                  <div className="border-b-2 border-black w-40 mt-5 ">
-                    Bay Area
-                  </div>
-
-                  <div className="border-b-2 border-black w-40 mt-5 ">WHEN:</div>
-
-                  <div className="border-b-2 border-black w-40 mt-5 ">April</div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className="relative ">
-              <Image
-                src={hero}
-                alt="hero"
-                className="ml-auto mr-auto cursor-pointer"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              />
-              <div className="absolute bottom-0 right-0 translate-y-[-50%] p-2 text-white w-[45%]">
-                <p className="font-bold sm:text-[42px] paytone">WELCOME TO</p>
-                <p className="font-bold sm:text-[52px] unite paytone">UNITE HACKS</p>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
+      <Flipper />
       <Timer />
       <Community />
       <Schedule />
