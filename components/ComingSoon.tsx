4f75f1b22be38255db44898aa35fe11efa1df119
dav <usr21p1@gmail.com>
2023-08-09 19:34:09 -0700

    Fix ComingSoon.tsx

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -4,7 +4,7 @@ const Timer = () => {
       <div className="flex py-10 justify-center bg-white">
         <div className="flex items-center">
           <span className="sm:text-[30px] py-1 text-xl font-bold time paytone">
-            September 9th, 2023 - September 10th, 2023
+            September 9th, 2023
           </span>
         </div>
       </div>
